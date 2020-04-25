decd8a7d8cbe4d8fd6fc6dcd9d26147ddfa4b4ea
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-04-25 18:09:58 +0530

    added morgan

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
+
+// Add morgan logging
+app.use(morgan(":method :url :status :res[content-length] :response-time ms"));
+
+// Use config env
 
 app.get('/', (req, res) => {
     res.json({

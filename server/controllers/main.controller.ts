1bb7437bcb7f02db7359a82b730127f6e6d434f5
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-04-25 19:41:22 +0530

    added default response to main controller

--- server/controllers/main.controller.ts
+++ server/controllers/main.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 
 export const MainController = {
     index: (req: Request, res: Response) => {
-        return res.json()
+        return res.json({
+            message: "App is live"
+        })
     }
 }
